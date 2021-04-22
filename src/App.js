5cb93cc000f0b34dd620eb062afb3e03e3b953b9
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-04-22 10:33:51 -0600

    Add checkbox and FormControlLabel to project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 import './App.css';
-import { Button, ButtonGroup } from '@material-ui/core';
+import { useState } from 'react';
+import {
+  Button,
+  ButtonGroup,
+  Checkbox,
+  FormControlLabel,
+} from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
+
+function CheckboxExample() {
+  const [checked, setChecked] = useState(true);
+  return (
+    <FormControlLabel
+      label='Testing Checkbox'
+      control={
+        <Checkbox
+          icon={<DeleteIcon />}
+          checkedIcon={<SaveIcon />}
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          imputProps={{ 'aria-lable': 'secondary checkbox' }}
+        />
+      }
+    />
+  );
+}
 
 function App() {
   return (
     <div className='App'>
       <header>React Material UI Tutorial</header>
-      <ButtonGroup 
-        variant='contained'
-        color='primary'
-        >
+      <CheckboxExample></CheckboxExample>
+      <ButtonGroup variant='contained' color='primary'>
         <Button
           startIcon={<SaveIcon />}
-          onClick={() => alert('hello')}
+          onClick={() => alert('saved')}
           size='large'
-          >
+        >
           Save
         </Button>
         <Button
           startIcon={<DeleteIcon />}
-          onClick={() => alert('hello')}
+          onClick={() => alert('deleted')}
           size='large'
-          >
+        >
           Discard
         </Button>
       </ButtonGroup>
-
     </div>
   );
 }
