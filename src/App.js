cb3d645771fa645a539f9de2d21df70602e0eaaf
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-04-22 09:43:48 -0600

    Add first button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <header>React Material UI Tutorial</header>
+      <Button
+        variant='contained'
+        color='primary'
+        onClick={() => alert('hello')}
+        size='large'
+      >
+        Hello world
+      </Button>
+    </div>
+  );
 }
 
 export default App;
